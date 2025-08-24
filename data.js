50efa5323a0b64ee1079da968700e62f369a05f1
Sihagjaat <amitsihag504@gmail.com>
2025-08-24 16:34:17 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
 // id: unique slug (lowercase, hyphens). Use this in share link: post.html?id=your-id
 const posts = [
   {
-    id: "banking-awareness-2025",
-    title: "Banking Awareness Notes 2025",
-    description: "Complete banking awareness PDF for IBPS, SBI, RRB. RBI, Monetary Policy, NBFCs, Payments.",
-    image: "https://via.placeholder.com/1200x600.png?text=Banking+Awareness+2025",
-    drive: "https://drive.google.com/your-file-id-1",
-    category: "Banking Awareness",
-    tags: ["RBI", "Monetary Policy", "Static GK"],
-    date: "2025-08-20",
-    contentHtml: "<h3>Inside this PDF</h3><ul><li>Evolution of banking</li><li>RBI structure & functions</li><li>Monetary policy tools</li></ul>"
+    id: "200+simplification-Questions",
+    title: "200+ Simplification Questions Pdf For Banking Exam",
+    description: "All Type of Simplification Questions ",
+    image: "https://ibb.co/cKYhCDNH",
+    drive: "https://drive.google.com/uc?id=1sHE5RZeXknmZ327VHv5hGFhMGH4giuR8&export=download",
+    category: "Quants",
+    tags: ["Quants", "Simplification", "Banking"],
+    date: "2025-08-24",
+    contentHtml: "<h3>Download Now</h3>"
   },
   {
-    id: "quant-short-tricks",
+    id: "quant-short-t"
     title: "Quant Short Tricks",
     description: "Speed tricks: base-100, near-squares, approximation and DI warmups.",
     image: "https://via.placeholder.com/1200x600.png?text=Quant+Tricks",
